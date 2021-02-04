b4534a9185e149a6c3c7c6a9ddf9cbc84bc2339a
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2021-02-04 12:48:06 +0000

    Create solution-prem.py

---
+++ level1/re-id/solution-prem.py
@@ -0,0 +1,23 @@
+def is_prime(b):    
+    for i in range(2,(int(b/2))):
+        if b%i==0: return False
+    return True
+
+def build_prime_str(n):
+    pstr = "23"
+    
+    b=4
+    while len(pstr)<=n+5:
+        b+=1
+        if is_prime(b): pstr += str(b)  
+        print(pstr, b)
+    
+    return pstr
+
+
+def solution(n):
+    pstr= build_prime_str(n)
+    print(pstr)
+        
+    return pstr[n:n+5]
+    
